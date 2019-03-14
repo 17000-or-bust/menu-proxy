84d6a890a429ca6a6ad67033c8b47ffd59f9971b
Chris Tso <christophertso2017@u.northwestern.edu>
2019-03-14 15:08:46 -0700

    Implemented proxy with deployed services

--- server.js
+++ server.js
@@ -11,31 +11,32 @@ let proxy = require('http-proxy-middleware');
 app.use(
   '/api/menu',
   proxy({
-    target: 'http://localhost:3000',
+    target: 'http://ec2-18-222-202-91.us-east-2.compute.amazonaws.com',
     changeOrigin: true
   })
  );
-// app.use(
-//   '/api/photos',
-//   proxy({
-//     target: 'http://ec2-18-206-121-61.compute-1.amazonaws.com',
-//     changeOrigin: true
-//   })
-// );
-// app.use(
-//   '/menu',
-//   proxy({
-//     target: 'http://ec2-3-17-28-103.us-east-2.compute.amazonaws.com/',
-//     changeOrigin: true
-//   })
-// );
-// app.use(
-//   '/overview',
-//   proxy({
-//     target: 'http://ec2-18-191-13-163.us-east-2.compute.amazonaws.com/',
-//     changeOrigin: true
-//   })
-// );
+app.use(
+  '/api/reserve',
+  proxy({
+    target: 'http://ec2-13-59-134-78.us-east-2.compute.amazonaws.com',
+    changeOrigin: true
+  })
+);
+app.use(
+  '/api/photos',
+  proxy({
+    target: 'http://ec2-54-159-114-20.compute-1.amazonaws.com',
+    changeOrigin: true
+  })
+);
+app.use(
+  '/overview',
+  proxy({
+    target: 'http://ec2-18-191-110-131.us-east-2.compute.amazonaws.com:3000',
+    changeOrigin: true
+  })
+);
+
 
 let morgan = require('morgan');
 app.use(morgan('dev'));
@@ -45,8 +46,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("*", (req, res) => {
 res.sendFile(__dirname + '/public/index.html')});  
-
-// app.use('/:id', express.static(__dirname + '/public'));
 
 app.listen(port, () => {
   console.log('Listening on port', port);
